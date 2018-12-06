8e4410b34f2fdb2c6998647677b4a5b50f2808e2
210951 Rydygier Konrad <210951@edu.p.lodz.pl>
2018-12-06 09:00:09 +0100

    Commit of finished Task 4

--- PresentationLayer/MainWindow.xaml.cs
+++ PresentationLayer/MainWindow.xaml.cs
@@ -27,6 +27,8 @@ namespace PresentationLayer
 
         ShopLogic shoplogic = new ShopLogic();
         string deleteThis;
+        string doWhat;
+        ClientViewer client;
 
         public MainWindow()
         {
@@ -34,9 +36,6 @@ namespace PresentationLayer
             shoplogic.ParseData(datagen.GiveData());
             InitializeComponent();
             updateLists();
-            //balance = Double.Parse(moneyBox.Text);
-            //loginPanel.Visibility = Visibility.Collapsed;
-            //mainPanel.Visibility = Visibility.Visible;
         }
 
         private void showProductsButton_Click(object sender, RoutedEventArgs e)
@@ -49,6 +48,7 @@ namespace PresentationLayer
                 items.Add(new ProductViewer() { Name = shoplogic.shop.Stock[i].Name, Quantity = 1, Price = shoplogic.shop.Stock[i].Price });
             }
             productView.ItemsSource = items;
+            viewType.Content = "Products";
             productView.Visibility = Visibility.Visible;
         }
 
@@ -62,6 +62,7 @@ namespace PresentationLayer
                 items.Add(new ClientViewer() { Name = shoplogic.shop.Clients[i].Name, Balance = shoplogic.shop.Clients[i].Money, BasketValue = shoplogic.ValueOfBasket(shoplogic.shop.Clients[i]) });
             }
             clientView.ItemsSource = items;
+            viewType.Content = "Clients";
             clientView.Visibility = Visibility.Visible;
         }
 
@@ -75,6 +76,7 @@ namespace PresentationLayer
                 items.Add(new EventViewer() { Name = shoplogic.shop.Events[i].Name, Value = "nothing" });
             }
             eventView.ItemsSource = items;
+            viewType.Content = "Events";
             eventView.Visibility = Visibility.Visible;
         }
 
@@ -86,14 +88,24 @@ namespace PresentationLayer
                 eventView.Visibility = Visibility.Collapsed;
                 clientView.Visibility = Visibility.Visible;
                 showWarningLabel("You need to be in Client view!");
-
             }
             else
             {
-
                 ClientViewer clientToDelete = (ClientViewer)clientView.SelectedItem;
-                shoplogic.LeaveShopp(new Client(0, clientToDelete.Name));
-                updateLists();
+                for (int i = 0; i < shoplogic.shop.Clients.Count(); i++)
+                {
+                    if(clientToDelete.Name == shoplogic.shop.Clients[i].Name)
+                    {
+                        if (shoplogic.Checkout(shoplogic.shop.Clients[i]))
+                        {
+                            updateLists();
+                        }
+                        else
+                        {
+                            showWarningLabel("You can't checkout " + clientToDelete.Name);
+                        }
+                    }
+                }
             }
         }
 
@@ -121,6 +133,7 @@ namespace PresentationLayer
             addPanel.Visibility = Visibility.Collapsed;
             mainPanel.Visibility = Visibility.Visible;
             updateLists();
+            success.Visibility = Visibility.Collapsed;
         }
 
         private void addButton_Click(object sender, RoutedEventArgs e)
@@ -133,21 +146,38 @@ namespace PresentationLayer
             if (deleteThis == "Client")
             {
                 shoplogic.EnterShop(new Client(double.Parse(moneyBox.Text), nameBox.Text));
-                moneyBox.Clear();
-                nameBox.Clear();
-                success.Visibility = Visibility.Visible;
+                success.Content = "Client successfully added";
             }
             else
             {
                 shoplogic.AddToStock(new Product(double.Parse(moneyBox.Text), nameBox.Text));
-                moneyBox.Clear();
-                nameBox.Clear();
-                success.Visibility = Visibility.Visible;
+                success.Content = "Product successfully added";
             }
-
+            moneyBox.Clear();
+            nameBox.Clear();
+            success.Visibility = Visibility.Visible;
         }
 
-        private void deleteClientButton_Click(object sender, RoutedEventArgs e)
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (eventView.Visibility == Visibility.Visible)
+            {
+                showWarningLabel("You need to be in Client or Product view!");
+            }
+            else if(clientView.Visibility == Visibility.Visible)
+            {
+                ClientViewer clientToDelete = (ClientViewer)clientView.SelectedItem;
+                shoplogic.LeaveShopp(new Client(0, clientToDelete.Name));
+            }
+            else
+            {
+                ProductViewer productToDelete = (ProductViewer)productView.SelectedItem;
+                shoplogic.RemoveFromStock(new Product(productToDelete.Price, productToDelete.Name));
+            }
+            updateLists();
+        }
+
+        private void addToBasketButton_Click(object sender, RoutedEventArgs e)
         {
             if (clientView.Visibility == Visibility.Collapsed)
             {
@@ -155,28 +185,77 @@ namespace PresentationLayer
                 eventView.Visibility = Visibility.Collapsed;
                 clientView.Visibility = Visibility.Visible;
                 showWarningLabel("You need to be in Client view!");
+
             }
             else
             {
-                ClientViewer clientToDelete = (ClientViewer)clientView.SelectedItem;
-                shoplogic.LeaveShopp(new Client(0, clientToDelete.Name));
+                client = (ClientViewer)clientView.SelectedItem;
+                clientLabel.Content = client.Name;
+                mainPanel.Visibility = Visibility.Collapsed;
+                productClientButton.Content = "Add";
+                doWhat = "Add";
+                productSelectPanel.Visibility = Visibility.Visible;
                 updateLists();
             }
         }
 
-        private void deleteProductButton_Click(object sender, RoutedEventArgs e)
+        private void productClientButton_Click(object sender, RoutedEventArgs e)
         {
-            if (productView.Visibility == Visibility.Collapsed)
+            ProductViewer product = (ProductViewer)productSelectView.SelectedItem;
+
+            for (int i = 0; i < shoplogic.shop.Clients.Count(); i++)
             {
-                clientView.Visibility = Visibility.Collapsed;
+                if (client.Name == shoplogic.shop.Clients[i].Name)
+                {
+                    if(doWhat == "Add")
+                    {
+                        for (int j = 0; j < shoplogic.shop.Stock.Count(); j++)
+                        {
+                            if (product.Name == shoplogic.shop.Stock[j].Name)
+                            {
+                                shoplogic.AddToBasket(shoplogic.shop.Clients[i], shoplogic.shop.Stock[j]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 0; j < shoplogic.shop.Clients[i].Basket.Count(); j++)
+                        {
+                            if (product.Name == shoplogic.shop.Clients[i].Basket[j].Name)
+                            {
+                                shoplogic.RemoveFromBasket(shoplogic.shop.Clients[i], shoplogic.shop.Clients[i].Basket[j]);
+                            }
+                        }
+                    }
+                }
+            }
+            updateLists();
+        }
+
+        private void backClientButton_Click(object sender, RoutedEventArgs e)
+        {
+            productSelectPanel.Visibility = Visibility.Collapsed;
+            mainPanel.Visibility = Visibility.Visible;
+        }
+
+        private void deleteFromBasketButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (clientView.Visibility == Visibility.Collapsed)
+            {
+                productView.Visibility = Visibility.Collapsed;
                 eventView.Visibility = Visibility.Collapsed;
-                productView.Visibility = Visibility.Visible;
-                showWarningLabel("You need to be in Product view!");
+                clientView.Visibility = Visibility.Visible;
+                showWarningLabel("You need to be in Client view!");
+
             }
             else
             {
-                ProductViewer productToDelete = (ProductViewer)productView.SelectedItem;
-                shoplogic.RemoveFromStock(new Product(productToDelete.Price, productToDelete.Name));
+                client = (ClientViewer)clientView.SelectedItem;
+                clientLabel.Content = client.Name;
+                mainPanel.Visibility = Visibility.Collapsed;
+                productClientButton.Content = "Delete";
+                doWhat = "Delete";
+                productSelectPanel.Visibility = Visibility.Visible;
                 updateLists();
             }
         }
@@ -202,6 +281,25 @@ namespace PresentationLayer
                 products.Add(new ProductViewer() { Name = shoplogic.shop.Stock[i].Name, Quantity = 1, Price = shoplogic.shop.Stock[i].Price });
             }
             productView.ItemsSource = products;
+            List<ProductViewer> products2 = new List<ProductViewer>();
+            if (doWhat == "Delete")
+            {
+                for (int i = 0; i < shoplogic.shop.Clients.Count(); i++)
+                {
+                    if (client.Name == shoplogic.shop.Clients[i].Name)
+                    {
+                        for (int j = 0; j < shoplogic.shop.Clients[i].Basket.Count(); j++)
+                        {
+                            products2.Add(new ProductViewer() { Name = shoplogic.shop.Clients[i].Basket[j].Name, Quantity = 1, Price = shoplogic.shop.Clients[i].Basket[j].Price });
+                        }
+                    }
+                }
+                productSelectView.ItemsSource = products2;
+            }
+            else
+            {
+                productSelectView.ItemsSource = products;
+            }
 
             List<ClientViewer> clients = new List<ClientViewer>();
             for (int i = 0; i < shoplogic.shop.Clients.Count(); i++)
@@ -248,7 +346,6 @@ namespace PresentationLayer
 
             public string Value { get; set; }
         }
-
 
     }
 }
